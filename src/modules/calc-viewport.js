007f47e5af7b727e891ab6491c49ecd1af1134e0
Sam A. Horvath-Hunt <samhh.web@gmail.com>
2017-08-17 10:38:06 +0100

    Ternary return in calc viewport

--- src/modules/calc-viewport.js
+++ src/modules/calc-viewport.js
@@ -6,16 +6,12 @@ const calcViewport = () => {
 	const width = Math.min(window.innerWidth, maxWidth)
 	const height = Math.min(window.innerHeight, maxHeight)
 
-	if (width >= height * aspectRatio) {
-		return {
-			width: Math.round(height * aspectRatio),
-			height
-		}
-	} else {
-		return {
-			width,
-			height: Math.round(width / aspectRatio)
-		}
+	return width >= height * aspectRatio ? {
+		width: Math.round(height * aspectRatio),
+		height
+	} : {
+		width,
+		height: Math.round(width / aspectRatio)
 	}
 }
 

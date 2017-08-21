33c675b22a92775309cf54a8f40fea395a3fd1f8
Sam A. Horvath-Hunt <samhh.web@gmail.com>
2017-08-21 15:37:53 +0100

    Add scenes; utilise Pixi ticker; add (functional) FPS counter

--- src/main.js
+++ src/main.js
@@ -2,11 +2,15 @@ import * as Pixi from 'pixi.js'
 import Keyboard from './modules/keyboard'
 import calcViewport from './modules/calc-viewport'
 import logLoadProgress from './modules/log-load-progress'
+import sleep from './modules/sleep'
 
 const {
 	Container,
+	Graphics,
 	WebGLRenderer: Renderer,
-	Sprite
+	Sprite,
+	Text,
+	ticker: { Ticker }
 } = Pixi
 
 const loader = new Pixi.loaders.Loader('/static/img/client/')
@@ -45,46 +49,128 @@ export default selector => {
 		.on('progress', logLoadProgress)
 		.load(setup)
 
-	// Models
-	let redBall1
-
 	// Setup
 	const kb = new Keyboard()
 	let state
+	let ticker
+
+	let pregameScene
+	let pregameText
+
+	let playScene
+	let fpsCounter
+	let redBall1
+
+	let endgameScene
+	let endgameText
 
 	function setup () {
 		const { width: viewWidth, height: viewHeight } = renderer
 
+		// Play scene
+		playScene = new Container()
+		playScene.visible = false
+		stage.addChild(playScene)
+
+		// FPS counter
+		fpsCounter = new Text('', {
+			fontFamily: 'Courier',
+			fontSize: 12,
+			fill: 'white'
+		})
+		fpsCounter.position.set(10, 10)
+		playScene.addChild(fpsCounter)
+
+		// Our first ball!
 		redBall1 = new Sprite(
 			loader.resources['ball-red'].texture
 		)
 		redBall1.width = 50
 		redBall1.height = 50
 		redBall1.anchor.set(0.5, 0.5)
-		redBall1.rotation = 5
 		redBall1.position.set(viewWidth / 2, viewHeight / 2)
+		playScene.addChild(redBall1)
 
-		stage.addChild(redBall1)
+		// Pregame scene
+		pregameScene = new Container()
+		stage.addChild(pregameScene)
 
-		state = play
+		const pregameBackground = new Graphics()
+		pregameBackground.beginFill(0x0000FF, 0.3)
+		pregameBackground.drawRect(0, 0, viewWidth, viewHeight)
+		pregameScene.addChild(pregameBackground)
+
+		pregameText = new Text('Starting soon...', {
+			fontSize: 40,
+			fill: 'white'
+		})
+		pregameText.anchor.set(0.5, 0.5)
+		pregameText.position.set(viewWidth / 2, viewHeight / 2)
+		pregameScene.addChild(pregameText)
+
+		// Endgame scene
+		endgameScene = new Container()
+		endgameScene.visible = false
+		stage.addChild(endgameScene)
+
+		endgameText = new Text('Game over!', {
+			fontSize: 40,
+			fill: 'white'
+		})
+		endgameText.anchor.set(0.5, 0.5)
+		endgameText.position.set(viewWidth / 2, viewHeight / 2)
+		endgameScene.addChild(endgameText)
+
+		// Begin the game in pregame state, and after 2 seconds enter play state
+		state = pregame
+		sleep(2000).then(() => {
+			state = play
+
+			// After 10 seconds enter end state
+			sleep(10000).then(() => {
+				state = endgame
+			})
+		})
 
 		main()
 	}
 
 	// Main game loop
 	function main () {
-		// Loop (no delta time at the moment)
-		requestAnimationFrame(main)
+		ticker = new Ticker()
+		ticker.stop()
+		ticker.add(dt => {
+			state(dt)
 
-		state()
+			renderer.render(stage)
+		})
+		ticker.start()
+	}
 
-		renderer.render(stage)
+	function pregame () {
+		pregameScene.visible = true
+		playScene.visible = true
+		endgameScene.visible = false
 	}
 
 	function play () {
+		pregameScene.visible = false
+		playScene.visible = true
+		endgameScene.visible = false
+
+		fpsCounter.text = `FPS: ${Math.round(ticker.FPS)}`
+
 		if (kb.up) redBall1.y -= 1
 		if (kb.down) redBall1.y += 1
 		if (kb.left) redBall1.x -= 1
 		if (kb.right) redBall1.x += 1
+
+		redBall1.rotation += 0.05
+	}
+
+	function endgame () {
+		pregameScene.visible = false
+		playScene.visible = false
+		endgameScene.visible = true
 	}
 }

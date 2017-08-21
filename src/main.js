a592fa7be1fa91e1658715bb3991e43cefce6573
Sam A. Horvath-Hunt <samhh.web@gmail.com>
2017-08-21 13:30:36 +0100

    Extending Pixi skeleton

--- src/main.js
+++ src/main.js
@@ -1,10 +1,25 @@
 import * as Pixi from 'pixi.js'
 import calcViewport from './modules/calc-viewport'
+import logLoadProgress from './modules/log-load-progress'
+
+const {
+	Container,
+	WebGLRenderer: Renderer,
+	Sprite
+} = Pixi
+
+const loader = new Pixi.loaders.Loader('/static/img/client/')
+
+const assets = [{
+	name: 'ball-red',
+	url: 'ball-red.png'
+}]
 
 export default selector => {
+	// Create renderer
 	const { width, height } = calcViewport()
 
-	const r = new Pixi.WebGLRenderer({
+	const renderer = new Renderer({
 		width,
 		height,
 		antialias: true,
@@ -15,12 +30,54 @@ export default selector => {
 	window.addEventListener('resize', () => {
 		const { width, height } = calcViewport()
 
-		r.resize(width, height)
+		renderer.resize(width, height)
 	})
 
-	selector.appendChild(r.view)
+	// Add view to DOM
+	selector.appendChild(renderer.view)
 
-	const stage = new Pixi.Container()
+	// Create stage
+	const stage = new Container()
 
-	r.render(stage)
+	loader
+		.add(assets)
+		.on('progress', logLoadProgress)
+		.load(setup)
+
+	// Models
+	let redBall1
+
+	// Setup
+	let state
+
+	function setup () {
+		redBall1 = new Sprite(
+			loader.resources['ball-red'].texture
+		)
+		redBall1.width = 50
+		redBall1.height = 50
+		redBall1.anchor.set(0.5, 0.5)
+		redBall1.rotation = 0.5
+		redBall1.position.set(200, 200)
+
+		stage.addChild(redBall1)
+
+		state = play
+
+		main()
+	}
+
+	// Main game loop
+	function main () {
+		// Loop (no delta time at the moment)
+		requestAnimationFrame(main)
+
+		state()
+
+		renderer.render(stage)
+	}
+
+	function play () {
+		redBall1.x += 1
+	}
 }
